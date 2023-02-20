fb35cf27b9adc6fbb7287ae6cca3c669acd6b9b8
Andrew Neil Young <andrewneilyoung88@gmail.com>
2023-02-20 23:53:34 +1100

    style edits, and axios get request setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,52 @@
 import '../App.css';
-
-
 import Header from './Header';
 import Button from './Button';
 import SubscriptionForm from './SubscriptionForm';
+import { useEffect, useState } from "react";
+import axios from "axios"
+
+const API_URL = `http://localhost:3000/subscriptions.json`;
+
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data);
+}
+
+
+
+
 
 function App() {
+  const [subscriptions, setSubscriptions] = useState([])
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((data) => {
+      if (mounted) {
+        setSubscriptions(data);
+      }
+    });
+    return () => (mounted = false)
+  }, [])
+
   return (
     <div className="App">
       <Header />
       <div class="subscription-call">
-        <p>Get discount and come have fun!</p>
-        <p>Kids join free on adult memberships.</p>
+        
         {/* <Button name="SUBSCRIBE" /> */}
       </div>
       <div class="form-container">
-        <h3>ZOO<span>Newsletter</span></h3>
-        <SubscriptionForm />
+        <div>
+        <h3>ZOO<span>Weekly Newsletter</span></h3>
+        </div>
+        <div class="welcome">
+        <p>Receive a discount and come have some fun! Kids join free on an adult membership.</p>
+        </div>
+        
+        <SubscriptionForm subscriptions={subscriptions} />
+        
       </div>
+      
     </div>
   );
 }
--- src/components/SubscriptionForm.jsx
+++ src/components/SubscriptionForm.jsx
@@ -1,115 +1,137 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-import { topics } from '../utils/topics';
+import { topics } from "../utils/topics";
 
-export default function SubscriptionForm() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
+export default function SubscriptionForm(props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    console.log({ ...register("firstName") }, { ...register("lastName") }, { ...register("email") })
+  console.log(
+    { ...register("firstName") },
+    { ...register("lastName") },
+    { ...register("email") }
+  );
 
-    const onSubmit = (data) => {
-        console.log(data);
-    };
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
-    const { reset } = useForm();
+  const { reset } = useForm();
 
-    // subscription topics
-    const [checkedState, setCheckedState] = useState(
-        new Array(topics.length).fill(false)
-    )
+  // subscription topics
+  const [checkedState, setCheckedState] = useState(
+    new Array(topics.length).fill(false)
+  );
 
-    const handleOnChange = (position) => {
-        const updatedCheckedState = checkedState.map((item, index) =>
-            index === position ? !item : item
-        );
-        setCheckedState(updatedCheckedState)
-    }
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+  };
 
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label>First Name</label>
+          <input
+            type="text"
+            name="first-name"
+            placeholder="First name"
+            {...register("firstName", {
+              required: "First name is required.",
+              pattern: {
+                value: /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/,
+                message: "Must contain only letters.",
+              },
+            })}
+          />
+          {errors.firstName && (
+            <p className="error-msg">{errors.firstName.message}</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label>Last Name</label>
+          <input
+            type="text"
+            name="last-name"
+            placeholder="Last name"
+            {...register("lastName", {
+              required: "Last name is required.",
+              pattern: {
+                value: /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/,
+                message: "Must contain only letters.",
+              },
+            })}
+          />
+          {errors.lastName && (
+            <p className="error-msg">{errors.lastName.message}</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label>Email Address</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email address"
+            {...register("email", {
+              required: "Email is required.",
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: "Email is not valid.",
+              },
+            })}
+          />
+          {errors.email && <p className="error-msg">{errors.email.message}</p>}
+        </div>
+        <div className="topics-container">
+          <label>I would like to receive: </label>
+          <div>
+            <ul className="topics">
+              {topics.map((topic, index) => {
+                return (
+                  <li key={index}>
+                    <label htmlFor={`checkbox-${index}`} className="option">
+                      {topic}
+                      <input
+                        type="checkbox"
+                        id={`checkbox-${index}`}
+                        name={topic}
+                        value={topic}
+                        checked={checkedState[index]}
+                        onChange={() => handleOnChange(index)}
+                      />
+                      <span class="checkmark"></span>
+                    </label>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="form-control">
+          <button type="submit">Subscribe</button>
+        </div>
+      </form>
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-control">
-                <label>First Name</label>
-                <input
-                    type="text"
-                    name="first-name"
-                    placeholder='First name'
-                    {...register("firstName", {
-                        required: "First name is required.",
-                        pattern: {
-                            value: /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/,
-                            message: "Must contain only letters."
-                        }
-                    })}
-                />
-                {errors.firstName && <p className='error-msg'>{errors.firstName.message}</p>}
+      <div>
+        <h1> These are the current subscriptions</h1>
+        {props.subscriptions.map((subscriptions) => {
+          return (
+            <div key={subscriptions.id}>
+              <p>
+                {subscriptions.first_name}, {subscriptions.last_name},{" "}
+                {subscriptions.email}, {subscriptions.topics}
+              </p>
             </div>
-            <div className="form-control">
-                <label>Last Name</label>
-                <input
-                    type="text"
-                    name="last-name"
-                    placeholder='Last name'
-                    {...register("lastName", {
-                        required: "Last name is required.",
-                        pattern: {
-                            value: /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/,
-                            message: "Must contain only letters."
-                        }
-                    })}
-                />
-                {errors.lastName && <p className='error-msg'>{errors.lastName.message}</p>}
-            </div>
-            <div className="form-control">
-                <label>Email Address</label>
-                <input
-                    type="text"
-                    name="email"
-                    placeholder='Email address'
-                    {...register("email", {
-                        required: "Email is required.",
-                        pattern: {
-                            value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-                            message: "Email is not valid."
-                        },
-                    })}
-                />
-                {errors.email && <p className='error-msg'>{errors.email.message}</p>}
-
-            </div>
-            <div className='topics-container'>
-                <label>I would like to receive: </label>
-                <div>
-                    <ul className='topics'>
-                        {topics.map((topic, index) => {
-                            return (
-                                <li key={index} >
-                                    <label htmlFor={`checkbox-${index}`} className="option">{topic}
-                                        <input
-                                            type="checkbox"
-                                            id={`checkbox-${index}`}
-                                            name={topic}
-                                            value={topic}
-                                            checked={checkedState[index]}
-                                            onChange={() => handleOnChange(index)}
-                                        />
-                                        <span class="checkmark"></span>
-                                    </label>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </div>
-            </div>
-            <div className="form-control">
-                <button type="submit">Subscribe</button>
-            </div>
-
-        </form>
-    )
+          );
+        })}
+      </div>
+    </>
+  );
 }
